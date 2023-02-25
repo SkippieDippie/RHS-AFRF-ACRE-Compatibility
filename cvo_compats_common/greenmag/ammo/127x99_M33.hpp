18a27cb304a790ae198fdc1592a60df29c125ceb
SkippieDippie <skippiedipie@gmail.com>
2023-02-25 20:40:01 +0200

    Added more greenmag ammo types

---
+++ cvo_compats_common/greenmag/ammo/127x99_M33.hpp
@@ -0,0 +1,86 @@
+class AMMO_CLASS(127x99_M33_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
+	displayName = CESTRING(ammo,127x99_M33_1Rnd,name);
+	picture = "\z\greenmag\addons\main\data\127mm.paa";
+
+	scope = 2;
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 2.5;
+	};
+};
+
+class AMMO_CLASS(127x99_M33_10Rnd) : CVO_GreenMag_Ammo_Core_10Rnd {
+	displayName = CESTRING(ammo,127x99_M33_10Rnd,name);
+
+	scope = 2;
+	scopeArsenal=2;
+	scopeCurator=2;
+
+	greenmag_ammotype = QAMMO_CLASS(127x99_M33_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 25.1;
+	};
+};
+
+class AMMO_CLASS(127x99_M33_20Rnd) : CVO_GreenMag_Ammo_Core_20Rnd {
+	displayName = CESTRING(ammo,127x99_M33_20Rnd,name);
+
+	scope = 2;
+	scopeArsenal=2;
+	scopeCurator=2;
+
+	greenmag_ammotype = QAMMO_CLASS(127x99_M33_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 50.2;
+	};
+};
+
+class AMMO_CLASS(127x99_M33_30Rnd) : CVO_GreenMag_Ammo_Core_30Rnd {
+	displayName = CESTRING(ammo,127x99_M33_30Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(127x99_M33_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 75.5;
+	};
+};
+
+class AMMO_CLASS(127x99_M33_40Rnd) : CVO_GreenMag_Ammo_Core_40Rnd {
+	displayName = CESTRING(ammo,127x99_M33_40Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(127x99_M33_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 100.7;
+	};
+};
+
+class AMMO_CLASS(127x99_M33_50Rnd) : CVO_GreenMag_Ammo_Core_50Rnd {
+	displayName = CESTRING(ammo,127x99_M33_50Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(127x99_M33_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 125.9;
+	};
+};
+
+class AMMO_CLASS(127x99_M33_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
+	displayName = CESTRING(ammo,127x99_M33_60Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(127x99_M33_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 151.0;
+	};
+};
---
+++ cvo_compats_common/greenmag/ammo/762x25_Basic.hpp
@@ -0,0 +1,86 @@
+class AMMO_CLASS(762x25_Basic_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
+	displayName = CESTRING(ammo,762x25_Basic_1Rnd,name);
+	picture = "\z\greenmag\addons\main\data\9mm.paa";
+
+	scope = 2;
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 0.2;
+	};
+};
+
+class AMMO_CLASS(762x25_Basic_10Rnd) : CVO_GreenMag_Ammo_Core_10Rnd {
+	displayName = CESTRING(ammo,762x25_Basic_10Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(762x25_Basic_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 2.0;
+	};
+};
+
+class AMMO_CLASS(762x25_Basic_20Rnd) : CVO_GreenMag_Ammo_Core_20Rnd {
+	displayName = CESTRING(ammo,762x25_Basic_20Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(762x25_Basic_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 4.0;
+	};
+};
+
+class AMMO_CLASS(762x25_Basic_30Rnd) : CVO_GreenMag_Ammo_Core_30Rnd {
+	displayName = CESTRING(ammo,762x25_Basic_30Rnd,name);
+
+	scope = 2;
+	scopeArsenal=2;
+	scopeCurator=2;
+
+	greenmag_ammotype = QAMMO_CLASS(762x25_Basic_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 6.0;
+	};
+};
+
+class AMMO_CLASS(762x25_Basic_40Rnd) : CVO_GreenMag_Ammo_Core_40Rnd {
+	displayName = CESTRING(ammo,762x25_Basic_40Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(762x25_Basic_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 8.0;
+	};
+};
+
+class AMMO_CLASS(762x25_Basic_50Rnd) : CVO_GreenMag_Ammo_Core_50Rnd {
+	displayName = CESTRING(ammo,762x25_Basic_50Rnd,name);
+
+	scope = 2;
+
+	greenmag_ammotype = QAMMO_CLASS(762x25_Basic_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 10.0;
+	};
+};
+
+class AMMO_CLASS(762x25_Basic_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
+	displayName = CESTRING(ammo,762x25_Basic_60Rnd,name);
+
+	scope = 2;
+	scopeArsenal=2;
+	scopeCurator=2;
+
+	greenmag_ammotype = QAMMO_CLASS(762x25_Basic_1Rnd);
+
+	class ItemInfo : CBA_MiscItem_ItemInfo {
+		mass = 12.0;
+	};
+};
