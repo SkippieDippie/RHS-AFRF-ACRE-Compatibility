9946eef27200c104190fc8cb21a7eb0db820abc6
SkippieDippie <skippiedipie@gmail.com>
2023-02-27 21:11:25 +0200

    Added .300WM magazines compatibility

---
+++ cvo_compats_rhsusaf/greenmag/magazines/rhs_usaf_762x67.hpp
@@ -0,0 +1,6 @@
+class rhsusf_5Rnd_300winmag_xm2010 : CA_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x67_Mk248_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x67_Basic_1Rnd);
+	greenmag_needBelt = 0;
+	greenmag_canSpeedload = 0;
+};
