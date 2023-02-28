13bb52ad1cfe02fd1be8674de6d0b7ca8144b1a4
SkippieDippie <skippiedipie@gmail.com>
2023-03-01 01:54:08 +0200

    Fixed typo

--- cvo_compats_rhssaf/greenmag/magazines/rhs_saf_556x45.hpp
+++ cvo_compats_rhssaf/greenmag/magazines/rhs_saf_556x45.hpp
@@ -2,23 +2,23 @@ class 30Rnd_556x45_Stanag;
 
 
 class rhssaf_30rnd_556x45_TDIM_G36 : 30Rnd_556x45_Stanag {
-	greenmag_ammo = QAMMO_CLASS(556x45_M855_1);
+	greenmag_ammo = QAMMO_CLASS(556x45_M855_1Rnd);
 };
 
 class rhssaf_30rnd_556x45_EPR_G36 : 30Rnd_556x45_Stanag {
-	greenmag_ammo = QAMMO_CLASS(556x45_M855A1_1);
+	greenmag_ammo = QAMMO_CLASS(556x45_M855A1_1Rnd);
 };
 
 class rhssaf_100rnd_556x45_EPR_G36 : 30Rnd_556x45_Stanag {
-	greenmag_ammo = QAMMO_CLASS(556x45_M855A1_1);
+	greenmag_ammo = QAMMO_CLASS(556x45_M855A1_1Rnd);
 };
 
 class rhssaf_30rnd_556x45_MDIM_G36 : 30Rnd_556x45_Stanag {
-	greenmag_ammo = QAMMO_CLASS(556x45_M855A1_1);
+	greenmag_ammo = QAMMO_CLASS(556x45_M855A1_1Rnd);
 };
 
 class rhssaf_30rnd_556x45_Tracers_G36 : 30Rnd_556x45_Stanag {
-	greenmag_ammo = QAMMO_CLASS(556x45_M856A1_1);
+	greenmag_ammo = QAMMO_CLASS(556x45_M856A1_1Rnd);
 };
 
 class rhssaf_30rnd_556x45_SPR_G36 : 30Rnd_556x45_Stanag {
@@ -30,7 +30,7 @@ class rhssaf_30rnd_556x45_SOST_G36 : 30Rnd_556x45_Stanag {
 };
 
 class rhsgref_30rnd_556x45_m21: CA_Magazine {
-	greenmag_ammo = QAMMO_CLASS(556x45_M855_1);
+	greenmag_ammo = QAMMO_CLASS(556x45_M855_1Rnd);
 	greenmag_basicammo = "greenmag_ammo_556x45_basic_1Rnd";
 	greenmag_canSpeedload = 1;
 	greenmag_needBelt = 0;
