c6d8c2b17357c999bc3f0d609f80bfc4e58719e3
SkippieDippie <skippiedipie@gmail.com>
2023-02-28 22:46:02 +0200

    Added more 5,56x45 magazines compatibility

--- cvo_compats_rhssaf/greenmag/magazines/rhs_saf_556x45.hpp
+++ cvo_compats_rhssaf/greenmag/magazines/rhs_saf_556x45.hpp
@@ -28,3 +28,14 @@ class rhssaf_30rnd_556x45_SPR_G36 : 30Rnd_556x45_Stanag {
 class rhssaf_30rnd_556x45_SOST_G36 : 30Rnd_556x45_Stanag {
 	greenmag_ammo = "greenmag_ammo_556x45_mk318_1Rnd";
 };
+
+class rhsgref_30rnd_556x45_m21: CA_Magazine {
+	greenmag_ammo = QAMMO_CLASS(556x45_M855_1);
+	greenmag_basicammo = "greenmag_ammo_556x45_basic_1Rnd";
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class rhsgref_30rnd_556x45_m21_t: rhsgref_30rnd_556x45_m21 {
+	greenmag_ammo = QAMMO_CLASS(556x45_M856A1_1Rnd);
+};
