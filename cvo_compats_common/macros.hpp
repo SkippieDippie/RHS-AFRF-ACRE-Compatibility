58459b1a362d5afd69e476317856aab42c7a2411
SkippieDippie <skippiedipie@gmail.com>
2023-02-28 16:40:01 +0200

    Updated macros, fixed a typo

--- cvo_compats_common/macros.hpp
+++ cvo_compats_common/macros.hpp
@@ -6,20 +6,9 @@
 
 #define QUADRUPLES(var1,var2,var3,var4) var1##_##var2##_##var3##_##var4
 
-#if __has_include("\z\greenmag\addons\main\script_component.hpp")
-#define GREENMAG_LOADED 1
-#else
-#define GREENMAG_LOADED 0
-#endif
 
-#if __has_include("\rhsafrf\addons\rhs_main\script_component.hpp")
-#define RHS_AFRF_LOADED 1
-#else
-#define RHS_AFRF_LOADED 0
-#endif
+#define GREENMAG_LOADED __has_include("\z\greenmag\addons\main\script_component.hpp")
 
-#if __has_include("\rhsusf\addons\rhsusf_main\script_component.hpp")
-#define RHS_USAF_LOADED 1
-#else
-#define RHS_USAF_LOADED 0
-#endif
+#define RHS_AFRF_LOADED __has_include("\rhsafrf\addons\rhs_main\script_component.hpp")
+
+#define RHS_USAF_LOADED __has_include("\rhsusf\addons\rhsusf_main\script_component.hpp")
