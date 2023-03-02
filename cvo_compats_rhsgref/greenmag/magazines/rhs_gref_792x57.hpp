5ab66e12bce508acc990061b6f2e807ccb0083b7
SkippieDippie <skippiedipie@gmail.com>
2023-03-02 21:06:15 +0200

    Adjusted the RHS SAF GreenMag config to new values

--- cvo_compats_rhsgref/greenmag/magazines/rhs_gref_792x57.hpp
+++ cvo_compats_rhsgref/greenmag/magazines/rhs_gref_792x57.hpp
@@ -7,8 +7,8 @@ class rhsgref_5Rnd_792x57_kar98k: CA_Magazine {
 };
 
 class rhsgref_10Rnd_792x57_m76: CA_Magazine {
-	greenmag_ammo = QAMMO_CLASS(792x57_Tracer_1Rnd);
-	greenmag_basicammo = QAMMO_CLASS(792x57_Basic_1Rnd);
+	greenmag_ammo = QAMMO_CLASS(792x57_FMJ_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(792x57_FMJ_1Rnd);
 
 	greenmag_canSpeedload = 0;
 	greenmag_needBelt = 0;
