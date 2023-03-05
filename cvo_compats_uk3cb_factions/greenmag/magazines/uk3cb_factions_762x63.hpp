7f7deba577f8bf61850fd033690abbfdcb569325
SkippieDippie <skippiedipie@gmail.com>
2023-03-05 18:07:51 +0200

    Added .30-06 (7.62x63) magazines

---
+++ cvo_compats_uk3cb_factions/greenmag/magazines/uk3cb_factions_762x63.hpp
@@ -0,0 +1,74 @@
+// M1903 Springfield Magazines
+class UK3CB_M1903A1_3006_5rnd_Magazine: 30Rnd_556x45_Stanag {
+	greenmag_ammo = QAMMO_CLASS(762x63_FMJ_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_R: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_FMJ_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_G: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_FMJ_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_Y: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_FMJ_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_W: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_FMJ_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_RT: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_GT: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_YT: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_M1903A1_3006_5rnd_Magazine_WT: UK3CB_M1903A1_3006_5rnd_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x63_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(762x63_Basic_1Rnd);
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
