fcee0c3432103151de0cc1985719020a88748c53
SkippieDippie <skippiedipie@gmail.com>
2023-03-05 18:05:56 +0200

    Added 7.62x39 magazines

---
+++ cvo_compats_uk3cb_factions/greenmag/magazines/uk3cb_factions_762x39.hpp
@@ -0,0 +1,307 @@
+// AK Magazines
+class UK3CB_AK47_30Rnd_Magazine : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+
+class UK3CB_AK47_30Rnd_Magazine_G : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_G : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_AK47_30Rnd_Magazine_R : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_R : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_AK47_30Rnd_Magazine_Y : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_Y : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_AK47_30Rnd_Magazine_W : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_W : rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+
+class UK3CB_AK47_30Rnd_Magazine_GT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_GT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_AK47_30Rnd_Magazine_RT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_RT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_AK47_30Rnd_Magazine_YT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_YT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_AK47_30Rnd_Magazine_WT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_AK47_45Rnd_Magazine_WT : rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+
+// RPK Magazines
+class UK3CB_RPK_75rnd_762x39: rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_RPK_75rnd_762x39_R: rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_G: rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_Y: rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_W: rhs_30Rnd_762x39mm {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_RPK_75rnd_762x39_RM: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_GM: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_YM: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_WM: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_RPK_75rnd_762x39_RT: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_GT: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_YT: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_RPK_75rnd_762x39_WT: rhs_30Rnd_762x39mm_tracer {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+
+// SKS Magazines
+class uk3cb_10rnd_magazine_sks: CA_Magazine {
+	greenmag_ammo = QAMMO_CLASS(762x39_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class uk3cb_10rnd_magazine_sks_RT: uk3cb_10rnd_magazine_sks {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class uk3cb_10rnd_magazine_sks_YT: uk3cb_10rnd_magazine_sks {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class uk3cb_10rnd_magazine_sks_GT: uk3cb_10rnd_magazine_sks {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
+
+class uk3cb_10rnd_magazine_sks_WT: uk3cb_10rnd_magazine_sks {
+	greenmag_ammo = QAMMO_CLASS(762x39_Tracer_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_762x39_basic_1Rnd";
+
+	greenmag_canSpeedload = 0;
+	greenmag_needBelt = 0;
+};
