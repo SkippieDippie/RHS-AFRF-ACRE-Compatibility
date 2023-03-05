8f4a48bc63559fb539bf4bc53bc349df2cec49c3
SkippieDippie <skippiedipie@gmail.com>
2023-03-05 18:05:31 +0200

    Added 7.5x55 magazines

---
+++ cvo_compats_uk3cb_factions/greenmag/magazines/uk3cb_factions_75x55.hpp
@@ -0,0 +1,41 @@
+// Stgw-57 Magazines
+class UK3CB_STGW57_24rnd_75x55: 30Rnd_556x45_Stanag {
+	greenmag_ammo = QAMMO_CLASS(75x55_FMJ_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(75x55_Basic_1Rnd);
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_STGW57_24rnd_75x55_RT: UK3CB_STGW57_24rnd_75x55 {
+	greenmag_ammo = QAMMO_CLASS(75x55_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(75x55_Basic_1Rnd);
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_STGW57_24rnd_75x55_GT: UK3CB_STGW57_24rnd_75x55 {
+	greenmag_ammo = QAMMO_CLASS(75x55_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(75x55_Basic_1Rnd);
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_STGW57_24rnd_75x55_YT: UK3CB_STGW57_24rnd_75x55 {
+	greenmag_ammo = QAMMO_CLASS(75x55_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(75x55_Basic_1Rnd);
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_STGW57_24rnd_75x55_WT: UK3CB_STGW57_24rnd_75x55 {
+	greenmag_ammo = QAMMO_CLASS(75x55_Tracer_1Rnd);
+	greenmag_basicammo = QAMMO_CLASS(75x55_Basic_1Rnd);
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
