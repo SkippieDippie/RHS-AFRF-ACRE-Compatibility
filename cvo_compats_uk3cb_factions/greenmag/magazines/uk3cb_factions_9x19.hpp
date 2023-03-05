092af06c46d05c2717b71a63dbd0cde10f18a6e5
SkippieDippie <skippiedipie@gmail.com>
2023-03-05 17:58:30 +0200

    Added 9x19 magazines

---
+++ cvo_compats_uk3cb_factions/greenmag/magazines/uk3cb_factions_9x19.hpp
@@ -0,0 +1,118 @@
+// AUG Magazines
+class UK3CB_AUG_25Rnd_9x19mm_Magazine : rhsusf_mag_17Rnd_9x19_FMJ {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+
+// Browning Hi-Power Pistol Magazines
+class UK3CB_BHP_9_13Rnd : rhsusf_mag_17Rnd_9x19_FMJ {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+
+// CZ-75 Magazines
+class UK3CB_CZ75_9_20Rnd : rhsusf_mag_17Rnd_9x19_FMJ {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+
+// MP5 Magazines
+class UK3CB_MP5_30Rnd_9x19_Magazine: 30Rnd_9x21_Mag {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_MP5_30Rnd_9x19_Magazine_R: 30Rnd_9x21_Mag {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_MP5_30Rnd_9x19_Magazine_RT: UK3CB_MP5_30Rnd_9x19_Magazine_R {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+class UK3CB_MP5_30Rnd_9x19_Magazine_Y;
+class UK3CB_MP5_30Rnd_9x19_Magazine_YT: UK3CB_MP5_30Rnd_9x19_Magazine_Y {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+class UK3CB_MP5_30Rnd_9x19_Magazine_G;
+class UK3CB_MP5_30Rnd_9x19_Magazine_GT: UK3CB_MP5_30Rnd_9x19_Magazine_G {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+class UK3CB_MP5_30Rnd_9x19_Magazine_W;
+class UK3CB_MP5_30Rnd_9x19_Magazine_WT: UK3CB_MP5_30Rnd_9x19_Magazine_W {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+
+
+// STEN Magazines
+class UK3CB_Sten_34Rnd_Magazine: 30Rnd_9x21_Mag_SMG_02 {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+class UK3CB_Sten_34Rnd_Magazine_R: 30Rnd_9x21_Mag_SMG_02 {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
+
+
+class UK3CB_Sten_34Rnd_Magazine_RT: UK3CB_Sten_34Rnd_Magazine_R {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+class UK3CB_Sten_34Rnd_Magazine_Y;
+class UK3CB_Sten_34Rnd_Magazine_YT: UK3CB_Sten_34Rnd_Magazine_Y {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+class UK3CB_Sten_34Rnd_Magazine_G;
+class UK3CB_Sten_34Rnd_Magazine_GT: UK3CB_Sten_34Rnd_Magazine_G {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+class UK3CB_Sten_34Rnd_Magazine_W;
+class UK3CB_Sten_34Rnd_Magazine_WT: UK3CB_Sten_34Rnd_Magazine_W {
+	greenmag_ammo = "greenmag_ammo_9x19_tracer_1Rnd";
+};
+
+
+
+// USP Magazines
+class UK3CB_USP_9_15Rnd: rhsusf_mag_17Rnd_9x19_FMJ {
+	greenmag_ammo = QAMMO_CLASS(9x19_FMJ_1Rnd);
+	greenmag_basicammo = "greenmag_ammo_9x19_basic_1Rnd";
+
+	greenmag_canSpeedload = 1;
+	greenmag_needBelt = 0;
+};
